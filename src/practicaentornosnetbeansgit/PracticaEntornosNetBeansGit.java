57eec9b8b742ea874167517fdb4eae951ea7ccf2
david <david@LAPTOP-0P7D9FL5>
2019-03-19 19:43:45 +0100

    Pequeña modificación en rama fix

--- src/practicaentornosnetbeansgit/PracticaEntornosNetBeansGit.java
+++ src/practicaentornosnetbeansgit/PracticaEntornosNetBeansGit.java
@@ -9,6 +9,7 @@ public class PracticaEntornosNetBeansGit {
 
     public void run() {
         System.out.println("Práctica de David Cortijo Traba");
+        System.out.println("Estamos en la rama fix");
     }
     public static void main(String[] args) {
         new PracticaEntornosNetBeansGit().run();
