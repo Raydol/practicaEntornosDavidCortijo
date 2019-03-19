5fe80d6b4fe42181b1c4794636c6cb517a668d46
david <david@LAPTOP-0P7D9FL5>
2019-03-19 20:25:36 +0100

    Nuevo commit en el proyecto clonado

--- src/practicaentornosnetbeansgit/PracticaEntornosNetBeansGit.java
+++ src/practicaentornosnetbeansgit/PracticaEntornosNetBeansGit.java
@@ -10,6 +10,10 @@ public class PracticaEntornosNetBeansGit {
     public void run() {
         System.out.println("Práctica de David Cortijo Traba");
         System.out.println("Estamos en la rama fix");
+        System.out.println("Modificación hecha tras haber hecho un fork de "
+                + "mi repositorio con otra cuenta.");
+        System.out.println("Ahora haré un pull request que aceptaré con mi "
+                + "cuenta principal para añadir estos cambios");
     }
     public static void main(String[] args) {
         new PracticaEntornosNetBeansGit().run();
