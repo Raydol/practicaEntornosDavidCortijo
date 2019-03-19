1592c816d0c9e2da7f75100a5a5c7eb5c804488b
david <david@LAPTOP-0P7D9FL5>
2019-03-19 19:42:08 +0100

    Primer commit en master

---
+++ src/practicaentornosnetbeansgit/PracticaEntornosNetBeansGit.java
@@ -0,0 +1,17 @@
+
+package practicaentornosnetbeansgit;
+
+/**
+ *
+ * @author david cortijo
+ */
+public class PracticaEntornosNetBeansGit {
+
+    public void run() {
+        System.out.println("Práctica de David Cortijo Traba");
+    }
+    public static void main(String[] args) {
+        new PracticaEntornosNetBeansGit().run();
+    }
+    
+}
